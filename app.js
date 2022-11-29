fd8df2dd85013fd275962bddf97205d4cef0bd22
whoknowsi <echosmania@gmail.com>
2022-11-29 14:49:26 -0300

    Added testing api endpoint for part5

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 const middleware = require('./utils/middleware')
 
-
-mongoose.connect(config.DB_URI)
-    .then(() => {
-        logger.info("connected to MongoDB")
-    })
-    .catch((error) => {
-        logger.error("error connecting to MongoDB: ", error.message)
-    })
-
+mongoose
+	.connect(config.DB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connecting to MongoDB: ', error.message)
+	})
 
 app.use(cors())
 app.use(express.json())
@@ -28,6 +28,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+if (process.env.NODE_ENV === 'test') {
+	const testingRouter = require('./controllers/testing')
+	app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
